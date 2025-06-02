7aaa124e1a4e5841619561ad5280763307fcaf29
k.sai kiran <ksaikiranchary2606@gmail.com>
2025-06-02 22:40:24 +0530

    Given num is Harshad num or not

--- Practice.py
+++ Practice.py
@@ -1,9 +1,12 @@
-# Print 1 to n even and odd numbers
+#Given number is Harshad number or not 
 
-num = int(input("Enter a number: ")) 
+num = input("Enter a number :")
 
-for i in range(1, num + 1):
-    if i % 2 == 0:
-        print(f"{i} is even")
-    else:
-        print(f"{i} is odd") 
+s = 0 
+for i in num:
+    s = s + int(i) 
+
+if (int(num) % s == 0):
+    print(num, "is a Harshad number")
+else:
+    print(num, "is not a Harshad number")
